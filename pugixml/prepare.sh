4ecb44933237f4cb835b98b538e7629d3c75cffb
thientc <thientc@futag>
2022-11-10 00:49:41 +0300

    fix prepare script for pugixml

--- pugixml/prepare.sh
+++ pugixml/prepare.sh
@@ -3,7 +3,10 @@
 # Futag-tests (https://github.com/thientc/Futag-tests): testing repository for Futag.
 # This file is distributed under the GPL v3 license (https://www.gnu.org/licenses/gpl-3.0.en.html).
 
-wget http://github.com/zeux/pugixml/releases/download/v1.12/pugixml-1.12.tar.gz
+
+if [ ! -f pugixml-1.12.tar.gz ]; then
+    wget http://github.com/zeux/pugixml/releases/download/v1.12/pugixml-1.12.tar.gz
+fi
 tar xf pugixml-1.12.tar.gz
-mv pugixml-1.12 pugixml
+
 echo "-- [Futag]: Run python build.py for generating fuzz-drivers."
