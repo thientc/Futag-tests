6f7b684382b7568be55ea385fbde88764cf35ad4
thientc <thientc@futag>
2022-12-27 22:46:20 +0300

    Fix uppercase

--- Docker/Docker.build.sh
+++ Docker/Docker.build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --network=host --platform linux/x86_64 -t Futag_test_ubuntu22 -f Ubuntu22.Dockerfile .
+docker build --network=host --platform linux/x86_64 -t futag_test_ubuntu22 -f Ubuntu22.Dockerfile .
--- Docker/Docker.run.sh
+++ Docker/Docker.run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --platform linux/x86_64 --rm --privileged --network host -v `pwd`:/host --name Futag_test_ubuntu22 Futag_test_ubuntu22 /bin/bash
+docker run -it --platform linux/x86_64 --rm --privileged --network host -v `pwd`:/host --name futag_test_ubuntu22 Futag_test_ubuntu22 /bin/bash
