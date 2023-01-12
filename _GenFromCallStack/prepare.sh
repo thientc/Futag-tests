97843bd47f4e43bd09adf16281c8833c677dd9e5
thientc <thientc@futag>
2023-01-12 23:41:34 +0300

    add download link for Futag 1.3.1

--- _GenFromCallStack/prepare.sh
+++ _GenFromCallStack/prepare.sh
@@ -13,21 +13,25 @@ if [ ! -d json-c-json-c-0.16-20220414 ]; then
     rm -rf json-c-json-c-0.16-20220414
 fi
 tar xf json-c-0.16-20220414.tar.gz
-echo "-- [Futag]: Установите Питон-пакет futag.1.3.1.tar.gz:"
-echo "$ pip install futag.1.3.1.tar.gz"
-echo ""
-echo "-- [Futag]: Run Futag-test/get-Futag.sh to get the whole Futag system"
-echo ""
-echo "-- [Futag]: Getting futag-parse.py:"
-if [ ! -f futag-parse.py ]; then
-    wget https://raw.githubusercontent.com/ispras/Futag/FromTraceBack/src/python/futag-parse.py
+
+echo "-- [Futag]: Getting Futag 1.3.1:"
+if [ ! -f futag-llvm.1.3.1.tar.xz ]; then
+    wget https://github.com/ispras/Futag/releases/download/1.3.1/futag-llvm.1.3.1.tar.xz
 fi
-cp futag-parse.py ../futag-llvm/python-package/
+if [ ! -d futag-llvm ]; then
+    rm -rf futag-llvm
+fi
+echo "-- [Futag]: Extracting Futag 1.3.1 ... "
+tar xf futag-llvm.1.3.1.tar.xz
+
+echo "-- [Futag]: Установите Питон-пакет futag.1.3.1.tar.gz:"
+echo "$ pip install futag-llvm/python-package/futag-1.3.1.tar.gz"
+echo ""
 
 echo "-- [Futag]: Примеры использования:"
-echo "$ python ../futag-llvm/python-package/futag-parse.py <json-file> <path-to-library>"
+echo "$ python futag-llvm/python-package/futag-parse.py <json-file> <path-to-library>"
 echo ""
 echo "-- [Futag]: Для json-c:"
-echo "$ python ../futag-llvm/python-package/futag-parse.py libjson-c.json json-c-json-c-0.16-20220414"
+echo "$ python futag-llvm/python-package/futag-parse.py libjson-c.json json-c-json-c-0.16-20220414"
 echo "-- [Futag]: Для pugixml:"
-echo "$ python ../futag-llvm/python-package/futag-parse.py pugixml.json pugixml-1.12"
+echo "$ python futag-llvm/python-package/futag-parse.py pugixml.json pugixml-1.12"
