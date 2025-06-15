8ce96c2cb17f57bd22cbe404f5f04e374474286b
thientc <thientc84@gmail.com>
2025-06-15 16:55:10 +0000

    add link python package

--- get-Futag.sh
+++ get-Futag.sh
@@ -17,6 +17,7 @@ echo ""
 echo "-- [Futag]: Getting stable Futag package."
 if [ ! -f futag-llvm.2.0.4.tar.xz ]; then
     wget https://github.com/ispras/Futag/releases/download/v3.0.0/futag-llvm18.alt11.tar.xz
+    wget https://github.com/ispras/Futag/releases/download/v3.0.0/futag-3.0.0.tar.gz
 fi
 echo "-- [Futag]: Extracting..."
 tar xf futag-llvm18.alt11.tar.xz
